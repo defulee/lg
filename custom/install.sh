36969986581c42ce13ebd5c46f3e7c59f027f41e
defu <defu@staff.weibo.com>
2019-10-05 16:58:14 +0800

    add gitignore

--- custom/install.sh
+++ custom/install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 current_path=$(pwd)
+test -d "${current_path}/custom/tools" || mkdir -p ${current_path}/custom/tools
 
 # install alibaba arthas
 echo -e "$(tput bold)install alibaba arthas...$(tput sgr0)"
 curl -L https://alibaba.github.io/arthas/install.sh | sh
-sed -i '' 's/.\/as.sh/${0}/g' as.sh
+sed -i '' 's/.\/as.sh/arthas/g' as.sh
 mv as.sh ${current_path}/custom/tools/arthas
