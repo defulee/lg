f91bcae366e66aa2c8db353bc2f70819e71788b1
defu <defu.ldf@qq.com>
2020-05-26 15:08:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if command -v git &> /dev/null; then
-	[ ! -d "moto" ] &&
-	git clone --depth 1 https://github.com/defulee/moto.git 
-    cd moto
+	[ ! -d "qu" ] &&
+	git clone --depth 1 https://github.com/defulee/qu.git 
+    cd qu
 
     echo -e "$(tput bold)prepare install custom tools...$(tput sgr0)"
     ./custom/install.sh
 
     echo -e "$(tput bold)prepare install custom tools...$(tput sgr0)"
     make install
-    source moto-complete
+    source qu-complete
 fi
